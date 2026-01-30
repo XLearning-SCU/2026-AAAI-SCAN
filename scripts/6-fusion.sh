83991d9cc075aa844e50e6b047c90c95eb40716f
XLearning Group <54518841+XLearning-SCU@users.noreply.github.com>
2026-01-30 19:46:43 +0800

    Update scripts
    Updated log file paths for ensemble runs to a new directory structure.

--- scripts/6-fusion.sh
+++ scripts/6-fusion.sh
@@ -9,14 +9,14 @@ python 6-fusion.py --dataset dog      --backbone clip-rn50 --use_ensemble 0 2>&1
 python 6-fusion.py --dataset sun      --backbone clip-rn50 --use_ensemble 0 2>&1 | tee logs/fusion/clip-rn50/sun.log
 
 mkdir -p logs/fusion/clip-rn50-ensemble
-python 6-fusion.py --dataset flower   --backbone clip-rn50 --use_ensemble 1 2>&1 | tee logs/fusion/clip-rn50/flower-ensemble.log
-python 6-fusion.py --dataset cub      --backbone clip-rn50 --use_ensemble 1 2>&1 | tee logs/fusion/clip-rn50/cub-ensemble.log
-python 6-fusion.py --dataset pet      --backbone clip-rn50 --use_ensemble 1 2>&1 | tee logs/fusion/clip-rn50/pet-ensemble.log
-python 6-fusion.py --dataset food     --backbone clip-rn50 --use_ensemble 1 2>&1 | tee logs/fusion/clip-rn50/food-ensemble.log
-python 6-fusion.py --dataset aircraft --backbone clip-rn50 --use_ensemble 1 2>&1 | tee logs/fusion/clip-rn50/aircraft-ensemble.log
-python 6-fusion.py --dataset car      --backbone clip-rn50 --use_ensemble 1 2>&1 | tee logs/fusion/clip-rn50/car-ensemble.log
-python 6-fusion.py --dataset dog      --backbone clip-rn50 --use_ensemble 1 2>&1 | tee logs/fusion/clip-rn50/dog-ensemble.log
-python 6-fusion.py --dataset sun      --backbone clip-rn50 --use_ensemble 1 2>&1 | tee logs/fusion/clip-rn50/sun-ensemble.log
+python 6-fusion.py --dataset flower   --backbone clip-rn50 --use_ensemble 1 2>&1 | tee logs/fusion/clip-rn50-ensemble/flower.log
+python 6-fusion.py --dataset cub      --backbone clip-rn50 --use_ensemble 1 2>&1 | tee logs/fusion/clip-rn50-ensemble/cub.log
+python 6-fusion.py --dataset pet      --backbone clip-rn50 --use_ensemble 1 2>&1 | tee logs/fusion/clip-rn50-ensemble/petlog
+python 6-fusion.py --dataset food     --backbone clip-rn50 --use_ensemble 1 2>&1 | tee logs/fusion/clip-rn50-ensemble/food.log
+python 6-fusion.py --dataset aircraft --backbone clip-rn50 --use_ensemble 1 2>&1 | tee logs/fusion/clip-rn50-ensemble/aircraft.log
+python 6-fusion.py --dataset car      --backbone clip-rn50 --use_ensemble 1 2>&1 | tee logs/fusion/clip-rn50-ensemble/car.log
+python 6-fusion.py --dataset dog      --backbone clip-rn50 --use_ensemble 1 2>&1 | tee logs/fusion/clip-rn50-ensemble/dog.log
+python 6-fusion.py --dataset sun      --backbone clip-rn50 --use_ensemble 1 2>&1 | tee logs/fusion/clip-rn50-ensemble/sun.log
 
 mkdir -p logs/fusion/clip-b16
 python 6-fusion.py --dataset flower   --backbone clip-b16 --use_ensemble 0 2>&1 | tee logs/fusion/clip-b16/flower.log
